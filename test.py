7b37c018e18b152142b1eda838b1450074ff6448
Marwan Mostafa <marawan206@gmail.com>
2025-02-25 00:00:00 +0200

    Added and updated test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import pytest
 import torch
-from face_cropper_node import MarwanFaceCropping
+from face_cropper_node import NodoFaceCropping
 
 def test_valid_crop():
-    node = MarwanFaceCropping()
+    node = NodoFaceCropping()
     image = torch.rand(1, 1024, 1024, 3)  # Batch size 1, square image
     cropped, = node.execute(image)
     
@@ -11,14 +11,14 @@ def test_valid_crop():
     assert cropped.shape[2] == int(1024 * (2/3)), "Width should be 2:3 ratio of height"
 
 def test_invalid_input_shape():
-    node = MarwanFaceCropping()
+    node = NodoFaceCropping()
     image = torch.rand(1024, 1024, 3)  # Missing batch dimension
     
     with pytest.raises(ValueError):
         node.execute(image)
 
 def test_non_square_input():
-    node = MarwanFaceCropping()
+    node = NodoFaceCropping()
     image = torch.rand(1, 800, 1200, 3)  # Non-square image
     cropped, = node.execute(image)
     
@@ -26,7 +26,7 @@ def test_non_square_input():
     assert cropped.shape[2] == int(800 * (2/3)), "Width should be 2:3 ratio of height"
 
 def test_small_image():
-    node = MarwanFaceCropping()
+    node = NodoFaceCropping()
     image = torch.rand(1, 300, 400, 3)  # Small image
     cropped, = node.execute(image)
     
@@ -34,7 +34,7 @@ def test_small_image():
     assert cropped.shape[2] == int(300 * (2/3)), "Width should be 2:3 ratio of height"
 
 def test_large_image():
-    node = MarwanFaceCropping()
+    node = NodoFaceCropping()
     image = torch.rand(1, 2048, 4096, 3)  # Large image
     cropped, = node.execute(image)
     
