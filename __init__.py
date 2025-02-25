a99247076c8477f35ba18189c32e91e5e243a14a
Marwan Mostafa <marawan206@gmail.com>
2025-02-26 00:00:00 +0200

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .face_cropper_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+print("ComfyUI-FaceCropping node loaded: Crops images to a 2:3 aspect ratio.")
